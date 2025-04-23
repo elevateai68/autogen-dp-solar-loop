758b2425a840da5355f57e1c40446d9e0888cb99
elevateai68 <elevateai68@gmail.com>
2025-04-23 17:52:04 -0400

    Create seo_writer.py

---
+++ agents/seo_writer.py
@@ -0,0 +1,3 @@
+# seo_writer.py
+def run_seo_writer(niche_summary):
+    return f"# {niche_summary}\n\nDiscover how this trend is changing the residential solar market and what you can do to benefit."
